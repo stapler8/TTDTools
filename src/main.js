957b86d14f82a93b98f3cdc1773271b47eb58c33
stapler8 <tait.brendan@gmail.com>
2024-07-02 19:07:46 -0400

    Test Commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+let weightInput = document.getElementById("weight");
+
+weightInput.addEventListener("change", weightOnChange);
+
+function weightOnChange()
+{
+    let weight = weightInput.value; // "100"
+    let outputWeight = parseInt(weight) * 2.2; // 100
+
+    let weightOutput = document.getElementById("weightOutput");
+    weightOutput.innerText = outputWeight.toString();
+}
