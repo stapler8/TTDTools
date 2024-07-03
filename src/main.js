b8a60f19f7a9d465b764688a02382951da87a451
CaptainMainsail <thomas.j.mcbrine@gmail.com>
2024-07-03 13:06:58 -0400

    refactored calculate function

--- src/main.js
+++ src/main.js
@@ -1,27 +1,42 @@
-let weightElement = document.getElementById("trainWeight");
-weightElement.addEventListener("change", calculateTractiveEffort);
+let trainWeightMetricElement = document.getElementById("trainWeightMetric");
+trainWeightMetricElement.addEventListener("change", calculate);
 
-let speedElement = document.getElementById("maxSpeed");
-speedElement.addEventListener("change", calculateTractiveEffort);
+let maxSpeedMetric1Element = document.getElementById("maxSpeedMetric1");
+maxSpeedMetric1Element.addEventListener("change", calculate);
 
 let angleElement = document.getElementById("inclineAngle");
-angleElement.addEventListener("change", calculateTractiveEffort);
+angleElement.addEventListener("change", calculate);
 
 let inclineLengthElement = document.getElementById("maxInclineLength");
-inclineLengthElement.addEventListener("change", calculateTractiveEffort);
+inclineLengthElement.addEventListener("change", calculate);
 
 let trainLengthElement = document.getElementById("trainLength");
-trainLengthElement.addEventListener("change", calculateTractiveEffort);
+trainLengthElement.addEventListener("change", calculate);
 
 
-function calculateTractiveEffort()
+function calculate()
 {
-	let weight = parseInt(weightElement.value);
-	let speed = parseFloat(speedElement.value) / 3.6;
+	let weight = parseInt(trainWeightMetricElement.value);
+	let speed = parseFloat(maxSpeedMetric1Element.value) / 3.6;
 	let angle = parseInt(angleElement.value);
 	let inclineLength = parseInt(inclineLengthElement.value);
 	let trainLength = parseInt(trainLengthElement.value);
+
 	
+	let tractiveEffort = calculateTractiveEffort(weight, angle, inclineLength, trainLength);
+	
+	let tractiveElement = document.getElementById("requiredEffort");
+	tractiveElement.innerText = tractiveEffort.toString() + "kN";	
+	
+	
+	let requiredPower = calculatePower(tractiveEffort, speed);
+	
+	let powerElement = document.getElementById("requiredPower");
+	powerElement.innerText = requiredPower.toString() + "kW";
+}
+
+function calculateTractiveEffort(weight, angle, inclineLength, trainLength)
+{
 	let flatEffort = weight * 35;
 	
 	let inclineEffort = 0;
@@ -33,13 +48,12 @@ function calculateTractiveEffort()
 	let tractiveEffort = flatEffort + inclineEffort;
 	tractiveEffort = Math.round(tractiveEffort) / 1000;
 	
-	let tractiveElement = document.getElementById("requiredEffort");
-	tractiveElement.innerText = tractiveEffort.toString() + "kN";	
-	
-	
+	return tractiveEffort;
+}
+
+function calculatePower(tractiveEffort, speed)
+{
 	let requiredPower = speed * tractiveEffort;
-	
 	requiredPower = Math.round(requiredPower);
-	let powerElement = document.getElementById("requiredPower");
-	powerElement.innerText = requiredPower.toString() + "kW";
+	return requiredPower;
 }
